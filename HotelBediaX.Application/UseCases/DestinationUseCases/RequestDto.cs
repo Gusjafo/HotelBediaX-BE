2fb36caf85a687e98c6679eb2e0137f8bcc77767
Gustavo <69098117+Gusjafo@users.noreply.github.com>
2025-09-07 17:28:33 +0200

    Add data annotations for destination requests

--- HotelBediaX.Application/UseCases/DestinationUseCases/RequestDto.cs
+++ HotelBediaX.Application/UseCases/DestinationUseCases/RequestDto.cs
@@ -1,17 +1,28 @@
-﻿using HotelBediaX.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using HotelBediaX.Domain.Enums;
 
 namespace HotelBediaX.Application.UseCases.DestinationUseCases
 {
     public class CreateDto
     {
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
+
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
         public required string CountryCode { get; set; }
+
+        [StringLength(500)]
         public string? Description { get; set; }
+
         public DestinationType Type { get; set; }
     }
 
     public class UpdateDto : CreateDto
     {
+        [Range(1, int.MaxValue)]
         public int Id { get; set; }
     }
 }
+
